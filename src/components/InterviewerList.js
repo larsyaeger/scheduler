5c9f1a10594a31150c0f6938124f082dd78fbbff
larsyaeger <larsyaeger@gmail.com>
2023-01-11 16:20:29 -0600

    mapped over to display all interviewers

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import './InterviewerList.scss'
+import InterviewerListItem from './InterviewerListItem';
 export default function InterviewerList(props) {
+  const list = props.interviewers.map(interviewer => {
+    return (
+      <InterviewerListItem
+      key={interviewer.id}
+      selected={props.interviewer === interviewer.id}
+      setInterviewer={props.setInterviewer}
+      {...interviewer}
+      />
+    )
+  })
   return (
     <section className="interviewers">
   <h4 className="interviewers__header text--light">Interviewer</h4>
-  <ul className="interviewers__list"></ul>
+  <ul className="interviewers__list">{list}</ul>
 </section>
 
   )
