87de8ad8382a1e0ee326d8fe5d844e4c9128fe51
larsyaeger <larsyaeger@gmail.com>
2023-01-10 22:13:27 -0600

    add extra cases to satisfy new tests

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import classNames from 'classnames';
 import "./DayListItem.scss";
+
+const formatSpots = spots => {
+  if (!spots) {
+    return 'no spots remaining'
+  }
+  if (spots === 1) {
+    return `${spots} spot remaining`
+  }
+  return `${spots} spots remaining`
+}
+
 export default function DayListItem(props) {
+const formattedMessage = formatSpots(props.spots)
+
 
   const dayClass = classNames('day-list__item', {
     'day-list__item--selected': props.selected,
@@ -11,8 +24,7 @@ export default function DayListItem(props) {
   return (
     <li className={dayClass }onClick={() => props.setDay(props.name)}>
       <h2 className={dayClass}>{props.name}</h2>
-      <h3 className={dayClass}>{props.spots} spots remaining</h3>
+      <h3 className={dayClass}>{formattedMessage}</h3>
     </li>
   );
   }
-  //   /Users/larsyaeger/lighthouse/scheduler/src/components/DayListItem.scss
