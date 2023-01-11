db5cf6998d9c00769b7fd033c9b39cfe7a346c21
larsyaeger <larsyaeger@gmail.com>
2023-01-11 11:24:06 -0600

    schedule mapping

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,7 +2,17 @@ import React from 'react';
 import DayListItem from './DayListItem';
 
 export default function DayList(props) {
+  const schedule = props.days.map(date => {
+    return (
+      <DayListItem
+      key={date.id}
+      selected={date.name === props.day}
+      setDay={props.setDay}
+      {...date}
+      />
+    )
+  })
   return (
-    <ul></ul>
+    <ul>{schedule}</ul>
   )
 }
