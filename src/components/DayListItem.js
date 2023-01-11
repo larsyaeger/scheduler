dfeac85b3326ded4a686af9f5f4fc3dcb09bfecc
larsyaeger <larsyaeger@gmail.com>
2023-01-10 21:36:26 -0600

    use classnames, make styling dynamic

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,18 @@
 import React from 'react';
-
+import classNames from 'classnames';
+import "./DayListItem.scss";
 export default function DayListItem(props) {
+
+  const dayClass = classNames('day-list__item', {
+    'day-list__item--selected': props.selected,
+    'day-list__item--full': props.spots === 0
+  });
+
   return (
-    <li onClick={() => props.setDay(props.name)}>
-      <h2 className='text--regular'>{props.name}</h2>
-      <h3 className='text--light'>{props.spots} spots remaining</h3>
+    <li className={dayClass }onClick={() => props.setDay(props.name)}>
+      <h2 className={dayClass}>{props.name}</h2>
+      <h3 className={dayClass}>{props.spots} spots remaining</h3>
     </li>
   );
-}
+  }
+  //   /Users/larsyaeger/lighthouse/scheduler/src/components/DayListItem.scss
