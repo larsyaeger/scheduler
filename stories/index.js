5d502e8d57787214aa02ae6e9dec26101088a7ac
larsyaeger <larsyaeger@gmail.com>
2023-01-11 18:52:44 -0600

    import appointment

--- stories/index.js
+++ stories/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Appointment from "components/Appointment/index.js";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 import DayListItem from "components/DayListItem";
