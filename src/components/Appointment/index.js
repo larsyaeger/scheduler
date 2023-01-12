68a089da22851da69ac3f9ccc2834e3ca4418ee0
larsyaeger <larsyaeger@gmail.com>
2023-01-12 16:57:23 -0600

    added some stuff thatll get returned

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,10 +1,14 @@
-import React, {Fragment} from 'react';
+import React from 'react';
 import './styles.scss';
 import Header from './Header';
 import Show from './Show';
 import Empty from './Empty';
 export default function Appointment(props) {
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time}></Header>
+      {props.interview && <Show/>}
+      {!props.interview && <Empty/>}
+    </article>
   );
 }
