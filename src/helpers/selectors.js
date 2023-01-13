4d780694740b9545d3285941eae04f55b723c400
larsyaeger <larsyaeger@gmail.com>
2023-01-12 23:39:59 -0600

    clean up the code

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -3,22 +3,13 @@ export function getAppointmentsForDay(state, day) {
   state.days.forEach((object) => {
     if (object.name === day) {
       object.appointments.forEach(appointment => {
-        for (const [appointmentNumber, appointmentObject] of  Object.entries(state.appointments)) {
-          //console.log(appointmentObject);
+        for (const [appointmentNumber, appointmentObject] of Object.entries(state.appointments)) {
           if (appointmentObject.id === appointment) {
-            dayArray.push(appointmentObject)
+            dayArray.push(appointmentObject);
           }
-          // state.appointments.forEach(appointmentObject => {
-          //   if (appointmentObject.key === appointment) {
-          //     dayArray.push(appointmentObject);
-          //   }
-          // });
-         } //dayArray.push(state.appointments);
-        });
-      //dayArray.push(object);
+        }
+      });
     }
   });
-  //console.log('dayArray under here!!!!!!!!!!!');
-  //console.log(dayArray);
   return dayArray;
 }
