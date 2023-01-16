849268fe897822fe521ffb35c6e9b1263d9de572
larsyaeger <larsyaeger@gmail.com>
2023-01-16 17:05:47 -0600

    saving mode, transitions, settimeout

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,14 +5,27 @@ import Show from './Show';
 import Empty from './Empty';
 import useVisualMode from 'hooks/useVisualMode';
 import Form from './Form';
+import Status from './Status';
 export default function Appointment(props) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
+  const SAVING = 'SAVING';
   //const interviewers = [];
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
+  function save(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer
+    };
+    transition(SAVING);
+    setTimeout(function() {
+      props.bookInterview(props.id, interview) // runs first
+      transition(SHOW) // runs second
+    }, 1000)
+  }
   return (
     <article className="appointment">
       <Header time={props.time} />
@@ -23,12 +36,17 @@ export default function Appointment(props) {
           interviewer={props.interview.interviewer}
         />
       )}
-      {mode === CREATE && (
+      {mode === CREATE &&(
         <Form
           interviewers={props.interviewers}
           onCancel={back}
+          onSave={save}
         />
       )}
+      {mode === SAVING && (
+        <Status/>
+      )}
+
     </article>
   );
 }
